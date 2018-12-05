d498dd8d514be1cda972b9b3f8f7503218c6df74
Phuc. Duong Chau Vinh <phuc.dcv@globaldesignit.vn>
2018-12-05 15:50:51 +0700

    update migration

--- server/models/user_account.js
+++ server/models/user_account.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User_account = sequelize.define('User_account', {
+  const User_account = sequelize.define('user_account', {
     user_profile_id: DataTypes.INTEGER,
     user_name: DataTypes.STRING,
     phone: DataTypes.STRING,
--- server/models/user_profile.js
+++ server/models/user_profile.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const user_profile = sequelize.define('user_profile', {
-    id: DataTypes.INTEGER
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    full_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    accept_tems_of_service: DataTypes.BOOLEAN,
+    time_zone: DataTypes.STRING
   }, {});
   user_profile.associate = function(models) {
     // associations can be defined here
