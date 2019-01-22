8cebe61f1d2ebde8e4137dab63b46ecf0b570d44
GLODEIT\Binh.LT <binh.lt@globaldesignit.vn>
2019-01-22 17:09:08 +0700

    Update server

---
+++ server/controllers/api-web/AuthController.js
@@ -0,0 +1,50 @@
+const models = require('../../models/');
+const jwt = require('jsonwebtoken');
+var crypto = require('crypto');
+
+const userCtrl = {
+    generateToken(user_id) {
+        return jwt.sign({ user: user_id }, process.env.SECRET_KEY_TOKEN);
+    },
+    /**
+     * Login system.
+     * 
+     * @return Response
+     */
+    async login(req, res, next) {
+        var errors = "";
+        var error_msg = [];
+        var pass = crypto.createHash('sha256').update(req.body.password).digest('hex');
+        let user = '';
+        if (errors) {
+            return error_msg = errors;
+        } else {
+            user = await models.user_accounts.findOne({ where : {email : req.body.email, password : pass}});
+            if (!user.get()) {
+                error_msg.push({param: 'login', msg: 'Login user or pass is wrong' });
+            } else if (user.status != 1) {
+                error_msg.push({ param: 'status', msg: 'Account not active'});
+            }
+        }
+        if (!error_msg.length) {
+            // then return a token, secret key should be an env variable
+            const token = userCtrl.generateToken(user.get().id);
+            res.cookie(process.env.COOKIES_KEY, token, { maxAge: parseInt(process.env.EXPIRES_COOKIES) });
+            res.status(200).send({
+                success_flg: true,
+                data: {
+                    id: user.get().id,
+                    email: user.get().email,
+                    phone: user.get().phone,
+                    token: token
+                }
+            });
+        } else {
+            res.status(403).send({ success_flg: false, errors: error_msg });
+        }
+        return;
+    },
+
+};
+
+module.exports = userCtrl;
--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ require('dotenv').config()
 const express = require('express')
 const next = require('next')
 
-const fs = require('fs')
-const path = require('path')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+const compression = require("compression")
+const session = require("express-session");
 const cookieParser = require('cookie-parser')
 const port = parseInt(process.env.PORT, 10) || 3000
 const env = process.env.NODE_ENV
 const dev = env !== 'production'
 const routes = require('../client/routes')
 const models = require('./models')
-const app = next({dir: './client', dev})
+const app = next({
+  dir: './client',
+  dev
+})
 
 const handler = routes.getRequestHandler(app)
 
@@ -23,57 +26,67 @@ app
   .then(() => {
     server = express()
 
-    server.use(helmet())
+    if (!dev) {
+      /* Helmet helps secure our app by setting various HTTP headers */
+      server.use(helmet());
+      /* Compression gives us gzip compression */
+      server.use(compression());
+    }
+
     server.use(bodyParser.json())
-    server.use(bodyParser.urlencoded({ extended: true }))
+    server.use(bodyParser.urlencoded({
+      extended: true
+    }))
     server.use(cookieParser())
     server.disable('x-powered-by')
 
-    require('./routes')(server, express)
+    server.use(session({
+      name: "starter_app",
+      // secret used for using signed cookies w/ the session
+      secret: process.env.SECRET_KEY_TOKEN,
+      // store: new MongoStore({
+      //   mongooseConnection: mongoose.connection,
+      //   ttl: 14 * 24 * 60 * 60 // save session for 14 days
+      // }),
+      // forces the session to be saved back to the store
+      // resave: false,
+      // don't save unmodified sessions
+      // saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 14 // expires in 14 days
+      }
+    }));
 
-    models.sequelize.sync().then(function() {
-        // Default catch-all handler to allow Next.js to handle all other routes
-        server.use(handler).listen(port)
-        server.on('error', onError);
-        server.on('listening', function() {
-            var addr = server.address();
-            var bind = typeof addr === 'string'
-              ? 'pipe ' + addr
-              : 'port ' + addr.port;
-            console.log('Listening on ' + bind);
-        });
+    server.use((req, res, next) => {
+      /* custom middleware to put our user data (from passport) on the req.user so we can access it as such anywhere in our app */
+      res.locals.user = req.user || null;
+      next();
     });
 
-})
-.catch(err => {
-console.log('An error occurred, unable to start the server')
-console.log(err)
-})
+    /* give all Next.js's requests to Next.js server */
+    server.get("/_next/*", (req, res) => {
+      handler(req, res);
+    });
 
-/**
- * Event listener for HTTP server "error" event.
- */
+    require('./routes')(server, express)
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+    /* Error handling from async / await functions */
+    server.use((err, req, res, next) => {
+      const {
+        status = 500, message
+      } = err;
+      res.status(status).json(message);
+    });
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+    models.sequelize.sync().then(function () {
+      // Default catch-all handler to allow Next.js to handle all other routes
+      server.use(handler).listen(port)
+      console.log('Listening on ' + port);
+    });
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+  })
+  .catch(err => {
+    console.log('An error occurred, unable to start the server')
+    console.log(err)
+  })
